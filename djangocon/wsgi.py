3fae7127045c7e0b7d8e15a6d23e7ace82955dd4
Jeff Triplett <jeff.triplett@gmail.com>
2015-03-06 12:44:31 -0600

    Adding a password to staging.

--- djangocon/wsgi.py
+++ djangocon/wsgi.py
@@ -1,6 +1,21 @@
 import os
 
+from barrel import cooper
+from django.core.wsgi import get_wsgi_application
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangocon.settings.local")
 
-from django.core.wsgi import get_wsgi_application
+username = os.environ.get('BARREL_USER')
+password = os.environ.get('BARREL_PASS')
+
 application = get_wsgi_application()
+
+if username and password:
+
+    auth_decorator = cooper.basicauth(
+        users=[(username, password), ],
+        realm='Password Protected'
+    )
+
+    application = auth_decorator(application)
