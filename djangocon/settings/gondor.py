920f1cd296e3a12a478d9631a9c8f19f5c13fcf9
Peter Baumgartner <pete@lincolnloop.com>
2015-03-09 09:20:47 -0600

    Test staging behind CDN
    Hosted under Lincoln Loop's Fastly account

--- djangocon/settings/gondor.py
+++ djangocon/settings/gondor.py
@@ -4,6 +4,7 @@ import urlparse
 from .base import *
 
 DEBUG = False
+GONDOR_INSTANCE = os.environ.get("GONDOR_INSTANCE", None)
 
 ALLOWED_HOSTS = [
     'cs998.gondor.co',
@@ -52,11 +53,18 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = False
 
 SITE_ID = int(os.environ.get("SITE_ID", "1"))
 
+if GONDOR_INSTANCE == 'develop':
+    CDN_URL = "http://staging.djangocon.us.global.prod.fastly.net/"
+else:
+    # TODO setup instance for prod
+    CDN_URL = "/"
+
+STATIC_URL = CDN_URL + "site_media/static/"
+MEDIA_URL = CDN_URL + "site_media/media/"
+
 MEDIA_ROOT = os.path.join(os.environ["GONDOR_DATA_DIR"], "site_media", "media")
 STATIC_ROOT = os.path.join(os.environ["GONDOR_DATA_DIR"], "site_media", "static")
 
-MEDIA_URL = "/site_media/media/"  # make sure this maps inside of site_media_url
-STATIC_URL = "/site_media/static/"  # make sure this maps inside of site_media_url
 ADMIN_MEDIA_PREFIX = STATIC_URL + "admin/"
 
 FILE_UPLOAD_PERMISSIONS = 0640
